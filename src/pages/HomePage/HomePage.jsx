74c7c982756d3627531c0747dde35600d5c33165
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-09-06 09:26:46 +0700

    feat: update HomePage layout and navigation to login

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import Button from '@/components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-    const {text} = styles;
+    const { text } = styles;
+    const navigate = useNavigate();
 
     return (
         <>
-        <div className={text}>
-            Home page
-        </div>
-
-        <Button content={"test primary"} variant={'secondary'}/>
-        <Button content={"test primary"} variant={'tertiary'}/>
-
+            <div className={text}>Home page</div>
+            <div
+                style={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '300px'
+                }}
+            >
+                {' '}
+                <div style={{ width: '250px' }}>
+                    <Button
+                        content={'page login'}
+                        variant={'secondary'}
+                        onClick={() => {
+                            navigate('/login');
+                        }}
+                    />
+                </div>
+            </div>
         </>
-        
     );
 };
 
